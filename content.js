1705a9373e4503dfd680b68858c108dad65a425f
auto-b64d <dmdjdj0213@gmail.com>
2022-03-12 05:00:27 +0900

    Add message: not loaded yet
    왜 duration 업뎃을 안했냐;

--- content.js
+++ content.js
@@ -2,11 +2,15 @@ const fillZero = n => n.toString().padStart(2, '0')
 
 chrome.runtime.onMessage.addListener(({ content }, _, sendResponse) => {
 	if (content !== 'download-start') return
+	if (getComputedStyle(document.querySelector('.fp-waiting')).display !== 'none') {
+		sendResponse({ content: 'video-not-loaded' })
+		return
+	}
 	const id = document.querySelector('meta[property="og:url"]').getAttribute('content').match(/videos\/([\w-]+)\/$/)[1]
 	const name = document.querySelector('.headline').children[0].innerHTML
 	const now = new Date()
 	const date = `${now.getFullYear()}${fillZero(now.getMonth() + 1)}${fillZero(now.getDate())}_${fillZero(now.getHours())}${fillZero(now.getMinutes())}${fillZero(now.getSeconds())}`
-	const duration = document.querySelectorAll('.title-description')[2].innerHTML
+	const duration = document.querySelectorAll('.title-description')[2].innerHTML.split(':').map(part => fillZero(parseInt(part))).join('_')
 	const uploader = document.querySelector('li > .author').innerHTML
 	chrome.storage.local.get('format', ({ format }) => {
 		const formatItems = [
--- popup.js
+++ popup.js
@@ -1,9 +1,12 @@
 chrome.tabs.query({ active: true, currentWindow: true }, ([ tab ]) => {
+	if (!/^https:\/\/thisvid\.com\/videos\/.+/.test(tab.url)) return
 	chrome.tabs.sendMessage(tab.id, { content: 'download-start' }, msg => {
-		console.log('msg', msg)
-		if (msg.content !== 'download-reply') return
-		chrome.downloads.download({ ...msg.value, conflictAction: 'uniquify' }, () => {
-			window.close()
-		})
+		if (msg.content === 'video-not-loaded') {
+			document.body.innerText = '아직 다운로드 불가능'
+		} else if (msg.content === 'download-reply') {
+			chrome.downloads.download({ ...msg.value, conflictAction: 'uniquify',  }, () => {
+				window.close()
+			})
+		}
 	})
 })
